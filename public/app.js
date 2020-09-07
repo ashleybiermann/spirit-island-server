200220391e81c5015546c5c62860e306c42f67c7
ASHLEY BIERMANN <ashleybiermann@gmail.com>
2020-09-07 12:45:06 -0700

    adds notes for adding a form to record data

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@
 $('document').ready(() => {
   $('#recordNewGame').click(function(e) {
     e.preventDefault();
-    window.location.href = '/games';
+    //TODO: toggle open a form that sits about the entire game data already rendered and on submit, it will add the data to the database and re-render all game data
+    // window.location.href = '/games'; FIXME: this probably doesn't need to be here
   });
 
   $('#viewAllGameData').click(function(e) {
